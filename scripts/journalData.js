ac70043959e32718b5975ff2991cb99351b37f9c
srcrank <salmacrank@gmail.com>
2021-03-08 09:21:27 -0600

    scripts

---
+++ scripts/journalData.js
@@ -0,0 +1,25 @@
+const journal = [
+    {
+        id: 1,
+        date: "07/24/2025",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS",
+        mood: "Happy"
+    },
+    {
+        id: 2,
+        date: "07/26/2025",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Frustrated"
+    }
+]
+
+
+export const getJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+        Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
